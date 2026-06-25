8b2094b56f4e578bd498e522e629f58572976231
coderlogic16-collab <coderlogic16@gmail.com>
2026-06-25 22:38:40 +0530

    Update SMART-HOME-AUTOMATION.ino
    uploaded the code for the part of web page

--- SMART-HOME-AUTOMATION.ino
+++ SMART-HOME-AUTOMATION.ino
@@ -1,6 +1,6 @@
 #include <WiFi.h>
-#include <Webserver.h>
-#include <Preference.h>
+#include <WebServer.h>
+#include <Preferences.h>
 #include "AdafruitIO_WiFi.h"
 
 //Wifi & Adafruit _IO
@@ -24,4 +24,50 @@ const int bulbpin  = 27;
 Webserver server(80) ;
 Preferences preferences;
 String mode = "AP";
+
+// Webpage
+
+const char HTML [] PROGMEM = R"rawliteral(
+<!DOCTYPE   html><html><head><title>ESP32  Control</title>
+<meta name = "viewport" content="width=device-width, intial-scale=1">
+<style>
+  body{text-align:centre;font-family:sans-serif;background-color:#f4f4f9;}
+  button{padding:14px;margin:8px;font-size:18px;border-radius:8px;border:none;background-color:#007bff;color:white;cursor:pointer;}
+  button;active{background-color:#0056b3;}
+  #status{margin-top:20px;font-weight:bold;color:#333;}
+</style>
+<script>
+  function sendDmd (url)  {
+    document.getElementById('status').innerText = 'Sending command...';}
+    fetch(url)
+      .then(response => response.text())
+      .then(text =>  {  document.getElementById( 'status').innerText = 'status: ' + text; })
+      .catch(err =>  {  document.getElemntById('status').innerText = 'Error connecting to ESP32'; });
+
+  }
+</script>
+</head><body>
+<h2>ESP32 Relay Control</h2>
+<p id="status">Status: Ready</p>
+<hr>
+<h3>Motor</h3>
+<button oneclick="sendCmd('/motor/on')">Motor ON</button>
+<button oneclick="sendCmd('/motor/off')">Motor OFF</button>
+<h3>Bulb</h3>
+<button oneclick="sendCmd('/bulb/on')">Bulb ON</button>
+<button oneclick="sendCmd('/bulb/off')">Bulb OFF</button>
+<h3>Object3</h3>
+<button oneclick="sendCmd('/object3/on')">Object3 ON</button>
+<button oneclick="sendCmd('/object3/off')">Object3 OFF</button>
+<hr>
+<h3>System Mode</h3>
+<button oneclick="sendCmd('/switchMode')" style="background-color:#dc3545; ">Switch to %MODE%</button>
+</body></html>
+)rawliteral";
+
+String processor(const String& var)  {
+  return (mode == "AP") ? "Station" : "AP";
+}
+
+
 
